301147e627909a10f73834fcb714668e01d86d2a
ricfield <ricfield@gmail.com>
2017-06-18 20:03:56 -0400

    Update sync_with_update_server.sh

--- sync_with_update_server.sh
+++ sync_with_update_server.sh
@@ -8,22 +8,25 @@ SFTP_SERVER_DIR=$2
 LOCAL_UPDATE_CACHE_DIR=$3
 
 
-# check for null command line parameter
-if [ -z $SFTP_SERVER ] 
+# check for null command line parameters
+if [ -z $SFTP_SERVER || -z $SFTP_SERVER_DIR || -z $LOCAL_UPDATE_CACHE_DIR ]
+then
    echo "Usage: sync_with_update_server.sh <server hostname or IP address> <directory path on server>"
    echo "       <local installer cache directory>"
    echo ""
-   echo "A trailing front slash (/) must be used in this program"
+   echo "A trailing front slash (/) must be used in this program for all directories."
    exit -1
 fi
 
 
 # Check input for errors
 if [ $(echo -n $SFTP_SERVER_DIR | tail -c 1) != "/" ]
+then
    echo "Error: server hostname directory path missing a trailing slash."
    exit -2 
 
 elif [ $(echo -n $LOCAL_UPDATE_CACHE_DIR | tail -c 1) != "/" ]
+then 
    echo "Error: local installer cache directory path missing a trailing slash."
    exit -3
    
@@ -33,7 +36,8 @@ else
 fi
 
 # check if synchronization process ran sucessfully
-if [ $? -eq 0 ] 
+if [ $? -eq 0 ]
+then
    echo "Update synchronization process completed normally"
 else
    echo "Synchronization process completed with errors"
