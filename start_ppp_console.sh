74c30eebdf07780806d131e863708080763407eb
ricfield <ricfield@gmail.com>
2017-06-29 23:27:25 -0400

    Update start_ppp_console.sh

--- start_ppp_console.sh
+++ start_ppp_console.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A simple script to get a ppp connection over a serial line console
-# 
+#
 # A simple use case for this is connecting an old computer that cannot
 # support a network card, but needs an Internet connection.
 
@@ -8,7 +8,7 @@
 # Use Microsoft DNS
 MS_DNS=Y
 
-# DNS Server IP Address 
+# DNS Server IP Address
 MS_DNS_IP_ADDRESS=172.16.0.254
 
 # Use CRTSCTS flow control
@@ -23,25 +23,28 @@ REMOTE_IP_ADDRESS=172.16.0.241
 
 
 # Check for input errors
-if [ $MS_DNS != "Y" || $MS_DNS != "N"]
+
+
+
+if [ $MS_DNS != "Y" ] && [ $MS_DNS != "N" ];
 then
    echo "A Microsoft DNS option is required.  Aborting..."
-   exit -2
-   
-elif [ $CRTSCTS != "Y" || $CRTSCTS != "N" ]
-then 
-   echo "A CRTSCTS options is required.  Aborting..."
-   exit -3
+   exit -1
 
-elif [ $PROXY_ARP != "Y" || $PROXY_ARP != "N" ]
+elif [ $CRTSCTS != "Y" ] && [ $CRTSCTS != "N" ];
+then
+   echo "A CRTSCTS options is required.  Aborting..."
+   exit -2
+
+elif [ $PROXY_ARP != "Y" ] && [ $PROXY_ARP != "N" ];
 then
    echo "A Proxy ARP options is required.  Aborting..."
-   exit -4
+   exit -3
 
-elif [ -z $LOCAL_IP || -z $REMOTE_IP ]
-then 
+elif [ -z $LOCAL_IP_ADDRESS ] || [ -z $REMOTE_IP_ADDRESS ];
+then
    echo "A local and remote IP address is required.  Aborting..."
-   exit -1
+   exit -4
 fi
 
 
@@ -49,23 +52,24 @@ fi
 
 pppd_command="pppd"
 
-if [ $MS_DNS == "Y" ]
+if [ $MS_DNS == "Y" ];
 then
-   pppd_command=$pppd_command" msdns"
+   pppd_command=$pppd_command" ms-dns $MS_DNS_IP_ADDRESS"
 fi
 
-if [ $CRTSCTS == "Y" ]
-then 
+if [ $CRTSCTS == "Y" ];
+then
    pppd_command=$pppd_command" crtscts"
 fi
 
-if [ $PROXY_ARP == "Y" ]
+if [ $PROXY_ARP == "Y" ];
 then
    pppd_command=$pppd_command" proxyarp"
 fi
 
+
 # Add remote and local IP addresses to the command
 
-ppp_command=$pppd_command" $LOCAL_IP_ADDRESS:$REMOTE_IP_ADDRESS
+pppd_command=$pppd_command" "$LOCAL_IP_ADDRESS":"$REMOTE_IP_ADDRESS
 
-echo $pppd_command
+$pppd_command
