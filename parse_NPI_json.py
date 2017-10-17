a9bd1c6343f3d65fb3c09f1304875b60071cc06a
ricfield <ricfield@gmail.com>
2017-10-16 21:23:38 -0400

    Build 101620172123

--- parse_NPI_json.py
+++ parse_NPI_json.py
@@ -1,15 +1,35 @@
 import urllib.request, json
 
-postal_code = input("Enter Postal Code: ")
-last_name = input("Enter Last Name: ")
+input_valid = True
+run = True
 
-nppes_url = urllib.request.urlopen("https://npiregistry.cms.hhs.gov/api/?limit=200&number=&enumeration_type=1&taxonomy_description=&last_name=" + last_name + "&firstname=&postal_code=" + postal_code)
+while run == True:
 
-pv_information_json = json.loads(nppes_url.read())
+   
+   postal_code = input("Enter Postal Code (Press enter for none): ")
+   last_name = input("Enter Last Name (Press enter for none): ")
+   first_name = input("Enter First Name  (Press enter for none): ")
 
-print("\n\nResults:\n")
+   nppes_url = urllib.request.urlopen("https://npiregistry.cms.hhs.gov/api/?limit=200&number=&enumeration_type=1&taxonomy_description=&last_name=" + last_name + "&firstname=" + first_name + "&postal_code=" + postal_code)
+
+   pv_information_json = json.loads(nppes_url.read())
+
+
+   
+   print("\n\nResults:\n")
+
+   print("NPI          Last Name                      First Name                   ")
+   print("-------------------------------------------------------------------------")
       
-for pv in pv_information_json['results']:
-   print("\nDoctor Name: " + pv['basic']['first_name'],pv['basic']['last_name'] + "\nNPI Number: " + str(pv['number']))
+      
+   for pv in pv_information_json['results']:
+      print('{:10}'.format(str(pv['number'])) + "   " + '{:31}'.format(pv['basic']['last_name']) + '{:30}'.format(pv['basic']['first_name']))
 
-print("\n")
+
+   print("\n")
+
+            
+         
+         
+      
+
